2085c3dce5ef2c856eb5bb0e9dc8d5974b5962a0
Pelin Ozel <67648249+pozel@users.noreply.github.com>
2023-03-29 16:33:05 -0400

    Update runall_DetectPeaks.m

--- runall_DetectPeaks.m
+++ runall_DetectPeaks.m
@@ -11,13 +11,13 @@ for i = 1:length(allFiles_str)
     %get ap.bin file
     if isempty(strfind(allFiles_str{i,1},'txt')) && ~isempty(strfind(allFiles_str{i,1},'ap.bin')) && ~isempty(strfind(allFiles_str{i,1},'tcat')) && isempty(strfind(allFiles_str{i,1},'peaks')) && isempty(strfind(allFiles_str{i,1},'meta'))
         tempFile = allFiles_str{i,1};
-        extractedpath = join([extractBefore(MetaFiles,'catgt') 'catgt' extractBetween(tempFile,'catgt','\') '\'],'');
+        extractedpath = join([extractBefore(tempFile,'catgt') 'catgt' extractBetween(tempFile,'catgt','\') '\'],'');
         fileName{k,1} = erase(tempFile,extractedpath{:});
         fileDir{k,1} = extractedpath{:};
         k=k+1;
     elseif ~isempty(strfind(allFiles_str{i,1},'siteCoords.txt')) && isempty(strfind(allFiles_str{i,1},'npy'))%get coordinate file
         tempFile2 = allFiles_str{i,1};
-        extractedpath2 = join([extractBefore(MetaFiles,'catgt') 'catgt' extractBetween(tempFile2,'catgt','\') '\'],'');
+        extractedpath2 = join([extractBefore(tempFile2,'catgt') 'catgt' extractBetween(tempFile2,'catgt','\') '\'],'');
         coordName{p,1} =  erase(tempFile2,extractedpath2{:});
         coordDir{p,1} = extractedpath2{:};
         p=p+1;
